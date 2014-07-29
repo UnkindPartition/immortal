396d0bd1b2285fd84a4e02b80d15bd2382072ba3
Roman Cheplyaka <roma@ro-che.info>
2014-07-29 09:56:38 +0300

    Haddocks

--- src/System/Immortal.hs
+++ src/System/Immortal.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables, MultiParamTypeClasses, FlexibleContexts #-}
+-- | This module is designed to be imported qualified, e.g.
+--
+-- >import qualified System.Immortal as Immortal
 module System.Immortal
   ( Thread
   , create
@@ -13,8 +16,13 @@ import Control.Monad.Trans.Control
 import Control.Concurrent.Lifted
 import Data.IORef
 
+-- | Immortal thread identifier (including its underlying 'ThreadId')
 data Thread = Thread ThreadId (IORef Bool)
 
+-- | Spawn a new immortal thread running the given computation.
+--
+-- If the computation ever finishes (either normally or due to an exception),
+-- it will be restarted (in the same thread).
 create :: MonadBaseControl IO m => m () -> m Thread
 create a = uninterruptibleMask $ \restore -> do
   stopRef <- liftBase $ newIORef False
@@ -26,10 +34,18 @@ create a = uninterruptibleMask $ \restore -> do
   pid <- fork go
   return $ Thread pid stopRef
 
+-- | Stop (kill) an immortal thread.
+--
+-- This is the only way to really stop an immortal thread.
 stop :: Thread -> IO ()
 stop (Thread pid stopRef) = do
   writeIORef stopRef True
   killThread pid
 
+-- | Get the 'ThreadId' of the immortal thread.
+--
+-- The 'ThreadId' can be used to throw asynchronous exception to interrupt
+-- the computation. This won't kill the thread, however — even if the
+-- exception is not handled, the computation will be simply restarted.
 threadId :: Thread -> ThreadId
 threadId (Thread pid _) = pid
